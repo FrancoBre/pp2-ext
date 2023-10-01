9317166dc75228ceddb0892adada0a4912993bff
FrancoBre <bregolif.fb@gmail.com>
2023-10-01 15:34:59 -0300

    fravega y garbarino mocks

--- src/main/java/org/ungs/FravegaScraper.java
+++ src/main/java/org/ungs/FravegaScraper.java
@@ -1,17 +1,16 @@
 package org.ungs;
 
+
 import shoppinator.core.interfaces.Scraper;
 
-public class MScraper implements Scraper {
+public class FravegaScraper extends Scraper {
+
+    FravegaScraper(String url) {
+        super.setUrl(url);
+    }
 
     @Override
     public String scrap(String productName) {
-        if (productName.isEmpty() || productName.equals("e")) {
-            return "[]";
-        }
-        if (productName.equals("a")) {
-            return "[{\"name\":\"a\",\"post_url\":\"https://example.com/\",\"product_presentation\":{\"price\":799.99,\"product_image_url\":\"https://example.com/\"}}]";
-        }
         if (productName.equals("featured")) {
             return "[{\"name\":\"i\",\"post_url\":\"https://example.com/\",\"product_presentation\":{\"price\":799.99,\"product_image_url\":\"https://example.com/\"}},"
                 + "{\"name\":\"j\",\"post_url\":\"https://example.com/\",\"product_presentation\":{\"price\":799.99,\"product_image_url\":\"https://example.com/\"}}]";
