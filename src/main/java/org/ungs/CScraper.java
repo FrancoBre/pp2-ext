bdd4de16d61905c2c6f46263bfd5a63d5513b2c2
FrancoBre <bregolif.fb@gmail.com>
2023-10-09 22:39:54 -0300

    cosas cosas cosas

---
+++ src/main/java/org/ungs/CScraper.java
@@ -0,0 +1,10 @@
+package org.ungs;
+
+import shoppinator.core.interfaces.Scraper;
+
+public class CScraper implements Scraper {
+
+    public String scrap(final String productName) {
+        return "[]";
+    }
+}
